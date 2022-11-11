2ab520d8760d11e9842027f1a53791660bf660e7
Deepesh Bhardwaj <70226498+deepesh16b@users.noreply.github.com>
2022-11-11 13:30:53 +0530

    Update BinaryTree.cpp

--- Trees/BinaryTree.cpp
+++ Trees/BinaryTree.cpp
@@ -288,6 +288,53 @@ void boundaryTraversal(node* root)
     cout<<endl;
 }
 
+//---Vertical Traversal of Tree---------------
+void verticalTraversal(node* root)
+{
+    vector<int> ans;
+    if(root == NULL)
+        return;
+    map<int, map<int, vector<int> > > mapping; 
+    //--hd = horizontal-distance, lvl = level, ans = final answer list
+    //--map<hd, map<lvl, vector<int> ans>> = mapping
+    queue< pair< node*, pair<int, int> > > q; 
+    //--queue< node* , pair(hd, lvl) >
+    q.push(make_pair(root, make_pair( 0 , 0 )));
+
+    while( !q.empty())
+    {
+        pair< node*, pair<int, int> > temp = q.front();
+        q.pop();
+        node* frontNode = temp.first;
+        int hd = temp.second.first;
+        int lvl = temp.second.second;
+        //now insert new node w.r.t. horiz.dist. & level
+        mapping[hd][lvl].push_back(frontNode->data);
+
+        if(frontNode->left)
+            q.push( make_pair( frontNode->left, make_pair( hd-1, lvl+1 ) ) );
+        if(frontNode->right)
+            q.push( make_pair( frontNode->right, make_pair( hd+1, lvl+1 ) ) );
+        
+
+    }
+    for(auto i: mapping)
+    {
+        for(auto j: i.second)
+        {
+            for(auto k: j.second)
+            {
+                ans.push_back(k);
+            }
+        }
+    }
+    for(auto i:ans)
+        cout<<i<<" ";
+    cout<<endl;
+
+}
+
+
 //------  1 2 3 4 5 6 7 -1 -1 -1 -1 8 -1 -1 -1 -1 -1 --------
 //------  1 2 3 4 5 -1 -1 -1 -1 -1 -1  ------------
 int main()
@@ -321,5 +368,6 @@ int main()
 
     zigzagTraversal(root);
     boundaryTraversal(root);
+    verticalTraversal(root);
     return 0;
 }
